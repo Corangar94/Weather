d426e087aadb97341d804078fb1ffaf983fa2bf5
ioanastelian94@gmail.com <Hakunamatata94Steauab94>
2020-09-21 01:53:56 +0300

    Daily weather, click listeners

--- app/src/main/java/com/example/elrondweather/View/WeatherDailyView.java
+++ app/src/main/java/com/example/elrondweather/View/WeatherDailyView.java
@@ -1,16 +1,27 @@
 package com.example.elrondweather.View;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 
+import com.example.elrondweather.Helper.WeatherClasses.JsonToJava.Weather;
+import com.example.elrondweather.R;
+
+import java.util.Calendar;
+
 import static com.example.elrondweather.R.layout.daily_weather_item;
 
-public class WeatherDailyView extends CardView{
-
+public class WeatherDailyView extends CardView implements View.OnClickListener{
+    TextView dayText, temperature;
+    ImageView icon;
+    int index;
 
     public WeatherDailyView(Context context) {
         super(context);
@@ -27,11 +38,44 @@ public class WeatherDailyView extends CardView{
     public WeatherDailyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context);
-
     }
 
     public void init(Context context) {
         LayoutInflater.from(context).inflate(daily_weather_item, this, true);
+        dayText = findViewById(R.id.dayText);
+        temperature = findViewById(R.id.temperature);
+        icon = findViewById(R.id.icon);
+        View view = findViewById(R.id.view);
+        view.setOnClickListener(onClick);
+    }
+
+    final OnClickListener onClick = v -> gotoNextPage(findViewById(R.id.view));
+
+    public void setDailyItem(Weather weather, int index){
+        this.index = index;
+        dayText.setText(getCurrentDay(index));
+        temperature.setText(String.valueOf(Math.round(weather.getDaily().get(index).getTemp().getDay())));
 
     }
+    public String getCurrentDay(int index){
+
+        String[] daysArray = {"Monday","Tuesday", "Wednesday","Thursday","Friday", "Saturday", "Sunday",};
+
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        day+=index-2;
+        return daysArray[day];
+    }
+
+    @Override
+    public void onClick(View v) {
+        gotoNextPage(v);
+    }
+
+    public void gotoNextPage(View v) {
+        Intent intent = new Intent(getContext(), ExtendedWeatherView.class);
+        intent.putExtra("SELECTED_DAY", index);
+        v.getContext().startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/elrondweather/View/WeatherItemView.java
+++ app/src/main/java/com/example/elrondweather/View/WeatherItemView.java
@@ -1,18 +1,26 @@
 package com.example.elrondweather.View;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+
+import com.example.elrondweather.Helper.CurrentLocation;
+import com.example.elrondweather.Helper.WeatherClasses.JsonToJava.Weather;
+import com.example.elrondweather.R;
 
 import java.util.ArrayList;
 
 import static com.example.elrondweather.R.layout.weather_item;
 
-public class WeatherItemView extends CardView {
+public class WeatherItemView extends CardView implements View.OnClickListener{
     private ArrayList<Integer> colorsArray;
+    TextView location,rainChance, temperature, humidity;
     public WeatherItemView(Context context) {
         super(context);
         init(context);
@@ -33,10 +41,28 @@ public class WeatherItemView extends CardView {
 
     public void init(Context context) {
         LayoutInflater.from(context).inflate(weather_item, this, true);
-
+        rainChance = findViewById(R.id.rainChance);
+        temperature = findViewById(R.id.temperature);
+        humidity = findViewById(R.id.humidity);
+        location = findViewById(R.id.dayText);
     }
 
-
-
-
+    public void setWeatherItem(Weather weather){
+        location.setText(CurrentLocation.getLocationName());
+        rainChance.setText(String.valueOf(Math.round(weather.getDaily().get(0).getPop())));
+        temperature.setText(String.valueOf(Math.round(weather.getDaily().get(0).getTemp().getDay())));
+        humidity.setText(String.valueOf(Math.round(weather.getDaily().get(0).getHumidity())));
+        View view = findViewById(R.id.view);
+        view.setOnClickListener(onClick);
+    }
+    @Override
+    public void onClick(View v) {
+        gotoNextPage(v);
+    }
+    final OnClickListener onClick = v -> gotoNextPage(findViewById(R.id.view));
+    public void gotoNextPage(View v) {
+        Intent intent = new Intent(getContext(), ExtendedWeatherView.class);
+        intent.putExtra("SELECTED_DAY", 0);
+        v.getContext().startActivity(intent);
+    }
 }
